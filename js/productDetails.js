adc6db32d660126f3808d14e7497a537d4f05b4f
abdo-coco <https://github.com/abdo-coco>
2025-05-11 01:17:17 +0300

    final commit

--- js/productDetails.js
+++ js/productDetails.js
@@ -6,6 +6,7 @@ window.addEventListener("DOMContentLoaded", () => {
   document.querySelector(".descriptionAnime").textContent = product.description;
   document.querySelector(".priceValue").textContent = product.price;
   document.querySelector(".Rating span").textContent = product.rating;
+
   let sizeValue = document.querySelectorAll(".valueSize");
   sizeValue.forEach((Element) => {
     Element.addEventListener("click", () => {
@@ -17,9 +18,13 @@ window.addEventListener("DOMContentLoaded", () => {
       Element.style.color = "#fff";
     });
   });
-  document.querySelector(".addd-to-cart").addEventListener("click", () => {
-    let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  let cartCounter = document.querySelector(".cart-value");
+  let counter = cart.length;
+  cartCounter.textContent = counter;
+
+  document.querySelector(".addd-to-cart").addEventListener("click", () => {
     const exists = cart.some(
       (item) => item.product_name === product.product_name
     );
@@ -27,35 +32,11 @@ window.addEventListener("DOMContentLoaded", () => {
     if (!exists) {
       cart.push(product);
       localStorage.setItem("cart", JSON.stringify(cart));
+      counter++;
+      cartCounter.textContent = counter;
       alert("Product added to cart successfully!");
+    } else {
+      alert("Product has been added once!");
     }
   });
-
-  // function updateHeader() {
-  //   const loginLink = document.querySelector(
-  //     '.main-nav-list a[href="login.html"]'
-  //   );
-  //   const signupLink = document.querySelector(
-  //     '.main-nav-list a[href="signup.html"]'
-  //   );
-  //   const userIcon = document.querySelector("#user-icon");
-  //   const logOut = document.querySelector(".log-out");
-  //   const cartIcon = document.querySelector(".cart-info");
-
-  //   if (localStorage.getItem("currentUser")) {
-  //     loginLink.style.display = "none";
-  //     signupLink.style.display = "none";
-  //     userIcon.style.display = "inline-block";
-  //     logOut.style.display = "inline-block";
-  //     cartIcon.style.display = "inline-block";
-  //   } else {
-  //     loginLink.style.display = "inline-block";
-  //     signupLink.style.display = "inline-block";
-  //     userIcon.style.display = "none";
-  //     cartIcon.style.display = "none";
-  //     logOut.style.display = "none";
-  //   }
-  // }
-
-  // updateHeader();
 });
